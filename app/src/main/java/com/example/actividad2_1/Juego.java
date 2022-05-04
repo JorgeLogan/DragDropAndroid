e35cd4b5318c88eb4bb464aa351ffb7944995fc9
JorgeLogan <jorgeacenal@gmail.com>
2022-05-04 23:33:16 +0200

    Sin fragments y con drag correcto en un view

--- app/src/main/java/com/example/actividad2_1/Juego.java
+++ app/src/main/java/com/example/actividad2_1/Juego.java
@@ -4,9 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.annotation.SuppressLint;
+import android.media.Image;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.DragEvent;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import java.security.spec.ECField;
 
@@ -17,23 +23,25 @@ public class Juego extends AppCompatActivity {
     FrameLayout frameSup;
     FrameLayout frameInf;
 
+    ImageView ivPrueba;
+
     @SuppressLint("NewApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_juego);
-
+/*
         // Cargamos los contenedores de los fragments
         this.frameSup = (FrameLayout)findViewById(R.id.frameFragSup);
         this.frameInf = (FrameLayout)findViewById(R.id.frameFragInf);
-
+*/
         // Cargamos los fragments
         try{
-            this.fragSuperior = new Columna(R.raw.logan0, R.raw.logan2, R.raw.logan3);
-            getSupportFragmentManager().beginTransaction().replace(R.id.frameFragSup, this.fragSuperior).commit();
+        //    this.fragSuperior = new Columna(R.raw.logan0, R.raw.logan2, R.raw.logan3);
+  //          getSupportFragmentManager().beginTransaction().replace(R.id.frameFragSup, this.fragSuperior).commit();
 
-            this.fragInferior = new Columna(R.raw.logan1, R.raw.logan4, R.raw.logan5);
-            getSupportFragmentManager().beginTransaction().replace(R.id.frameFragInf, this.fragInferior).commit();
+      //      this.fragInferior = new Columna(R.raw.logan1, R.raw.logan4, R.raw.logan5);
+    //        getSupportFragmentManager().beginTransaction().replace(R.id.frameFragInf, this.fragInferior).commit();
         }catch(Exception e){
             Log.d("Pruebas", e.getMessage());
         }
@@ -41,7 +49,30 @@ public class Juego extends AppCompatActivity {
         SonidoRecursos sonido = new SonidoRecursos(this, R.raw.sonido_ok, 2000);
         sonido.sonar();
 
+        ivPrueba = (ImageView) findViewById(R.id.ivPrueba);
+
+        ivPrueba.setOnTouchListener(new View.OnTouchListener() {
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if(motionEvent.getAction() == MotionEvent.ACTION_DOWN){
+                    Log.d("Pruebas", "Moviendo " + motionEvent.getAction() + " " + view.getX() + " " + motionEvent.getRawX());
+                    inicio_x = motionEvent.getX();
+                    inicio_y = motionEvent.getY();
+                }
+                else if(motionEvent.getAction() == MotionEvent.ACTION_MOVE){
+                    float motion_x = motionEvent.getX() - inicio_x;
+                    float motion_y = motionEvent.getY() - inicio_y;
+
+                    view.setX( view.getX() + motion_x);
+                    view.setY( view.getY() + motion_y);
+                }
+                return true;
+            }
+        });
     }
+
+    float inicio_x = 0,inicio_y = 0;
 
     private void cargarFragment(Fragment frag, int id){
 
