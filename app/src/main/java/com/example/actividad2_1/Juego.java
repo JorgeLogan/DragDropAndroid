cb7e508d041a1bac1b312abfab19208924a64721
JorgeLogan <jorgeacenal@gmail.com>
2022-05-05 20:48:38 +0200

    Interfaz mas sencilla y funcional. Los iconos de ficha no cambian

--- app/src/main/java/com/example/actividad2_1/Juego.java
+++ app/src/main/java/com/example/actividad2_1/Juego.java
@@ -23,16 +23,18 @@ public class Juego extends AppCompatActivity {
         setContentView(R.layout.activity_juego);
         SonidoRecursos sonido = new SonidoRecursos(this, R.raw.sonido_ok, 2000);
         sonido.sonar();
+        // Buscamos los image view de destino
         inDestino1 = (ImageView) findViewById(R.id.ivDestino1);
         inDestino2 = (ImageView) findViewById(R.id.ivDestino2);
         inDestino3 = (ImageView) findViewById(R.id.ivDestino3);
 
+        // Buscamos las fichas y les asignamos un destino
         ficha1 = (FichaJuego)findViewById(R.id.ficha1);
         ficha1.setImagenDestino(inDestino1);
         ficha2 = (FichaJuego)findViewById(R.id.ficha2);
-        ficha2.setImagenDestino(inDestino1);
+        ficha2.setImagenDestino(inDestino2);
         ficha3 = (FichaJuego)findViewById(R.id.ficha3);
-        ficha3.setImagenDestino(inDestino1);
+        ficha3.setImagenDestino(inDestino3);
 
         try{
             MiPaint miPaint = new MiPaint(this);
